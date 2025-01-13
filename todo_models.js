b3902da69769131baa1051b3d3f937f460d85c33
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2025-01-13 20:22:56 +0530

    Update todo_models.js

--- todo_models.js
+++ todo_models.js
@@ -1,1 +1,19 @@
 
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+ // username:String,
+ // email:String,
+ // isActive:Boolean,
+ // following syntax is modern way to write the schema object
+ usename:{
+  type:String,
+  required:true,
+  unique:true, //in which instgram unique user id 
+ },
+ password:{
+  type:String,
+  required:true,
+  unique:true, 
+ }
+});
+export const User=mongoose.model("User",userSchema)
